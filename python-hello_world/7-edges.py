3ebfbe8581bc802cacb5325cd55fdfb2ad6c9ba4
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-24 09:20:13 -0500

    Complete this source code

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word
+print(f"First 3 letters: {word_first_3[:-6]}")
+word_last_2 = word
+print(f"Last 2 letters: {word_last_2[7:]}")
+middle_word = word
+print(f"Middle word: {middle_word[1:]}")
