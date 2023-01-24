a1bdbe28618806b9517ab478920efd5cbdf6a06d
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-24 14:34:20 -0500

    add new varibale

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number == 0:
-    print(f"Last digit of {number} is {number} and is 0")
-elif number > 5:
-    print(f"Last digit of {number} is {number} and is greater than 5")
-else:
-    print(f"Last digit of {number} is {number} and is less than 6 and not 0")
+last_digit = number %10
+if last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} and is 0")
+elif last_digit < 6 and  last_digit != 0:
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+elif last_digit > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
