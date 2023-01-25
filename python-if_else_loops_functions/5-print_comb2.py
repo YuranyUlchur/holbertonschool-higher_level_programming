01a0900b8b477ef19d589601aeb83cb2e3de0ae5
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-24 22:37:49 -0500

    add space

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     print("{:02d}".format(number), end=", ")
-    if number == 99:
-        print(end=("{}".format("99\n")))
+print((f"{number}".format(number)), end= "\n")
