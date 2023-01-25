d90255127ec7d67cb7e12d5f6cfbffcea1dcf2cb
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-24 21:29:48 -0500

    program that prints numbers from 0 to 99.

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number < 10:
+        print(f"0{number}, ".format(number), end="")
+    else:
+        print(f"{number }, ".format(number), end="")
