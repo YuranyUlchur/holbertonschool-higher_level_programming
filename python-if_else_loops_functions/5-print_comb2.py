fe36e78ba35648579a489c564d8aedc16940e761
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-24 21:33:05 -0500

    delete coma

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,7 @@
 for number in range(0, 100):
     if number < 10:
         print(f"0{number}, ".format(number), end="")
+    elif number > 98:
+        print(f"{number}".format(number), end="")
     else:
         print(f"{number }, ".format(number), end="")
