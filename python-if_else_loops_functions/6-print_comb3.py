39d7aa2b241f19b07bdd7f2f29e81335ef0fa931
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-25 11:08:26 -0500

    function that checks for lowercase character.

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for number in range(0, 89):
+    if (number < 10):
+        print("{:02d}".format(number), end=", ")
+
---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def islower(c):
+    char = c.islower()
+    if (char):
+        return(True)
+    elif (char != 1):
+        return(False)
