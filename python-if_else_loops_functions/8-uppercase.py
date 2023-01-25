dc98cfe1f692988764fe0450999bb615393b9fbd
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-25 12:11:59 -0500

    prints a string in uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print(str.upper())
+    print("{}".format(str.upper()))
--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+def print_last_digit(number):
+     digit = number % 10
+     return digit
