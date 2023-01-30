987cc48982cf6a7170989ac5faa77be2f35dd7f8
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-30 17:34:05 -0500

    function that replaces an element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,5 +3,11 @@ def replace_in_list(my_list, idx, element):
     len_list = len(my_list)
     if idx < 0:
         return my_list
-    my_list[idx] = element
-    return my_list
+    if len_list == idx:
+        idx = element
+        return my_list
+    for x in range(0, len_list):
+        if x == idx:
+            my_list[x] = element
+            return my_list
+        
