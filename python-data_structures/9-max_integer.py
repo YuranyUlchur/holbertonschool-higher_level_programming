d28eeed24353aa33c582becf4159c9f524ca9700
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-01 15:43:55 -0500

    add funtions+

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    len_list = len(my_list)
-    if my_list == None:
-        return None
-    max_list = reduce(my_list)
-    print(max_list)
+    if my_list:max = my_list[0]
+    for x in my_list:
+        if x > max:
+            max = x
+            return max
+    return None
