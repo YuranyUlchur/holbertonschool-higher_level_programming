c337061a5409f4fab9bd4da5952f0a68fedd2cb2
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-01 10:12:30 -0500

    add identation

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,6 +3,5 @@ def max_integer(my_list=[]):
     len_list = len(my_list)
     if my_list == None:
         return None
-    for x in range(0, len_list):
-        if len_list > len_list[x] :
-            return(len_list[x])
+    max_list = reduce(my_list)
+    print(max_list)
