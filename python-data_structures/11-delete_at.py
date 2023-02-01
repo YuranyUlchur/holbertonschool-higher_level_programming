75e697ef5e3738e7d01951ef154a7381f554688b
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-01 09:49:21 -0500

    add changes

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,7 +3,7 @@ def delete_at(my_list=[], idx=0):
     if idx < 0:
         return my_list
     len_list = len(my_list)
-    for x in range(1, len_list):
+    for x in range(-1, len_list):
         if x == idx:
             del my_list[x]
             return my_list
