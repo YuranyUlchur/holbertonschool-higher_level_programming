a066e7d7ed7a44ba95c916567ba28b963985df49
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-02 11:56:54 -0500

    add functions

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple=()
-    if new_tuple < 2:
-        return f"{new_tuple:02d}"
-    if new_tuple > 2:
-        return f"{new_tuple:d}"
-    for x in tuple_a:
-        for i in tuple_b:
-           resul = (x + i)
-           new_tuple = (resul)
+    list_tuple = []
+    aux = 0
+    if len(tuple_a) == 0:
+        return tuple_b
+    if len(tuple_b) == 0:
+        return tuple_a
+    if len(tuple_b) == 1:
+        list_tuple.append(tuple_a[0] + tuple_b[0])
+        list_tuple.append(tuple_a[1] + 0)
+    elif len(tuple_a) == 1:
+        list_tuple.append(tuple_b[0] + tuple_a[0])
+        list_tuple.append(tuple_b[1] + 0)
+    else:
+        for x in range(0, len(tuple_a)):
+            for i in range(aux, len(tuple_b)):
+                resul = (tuple_a[x] + tuple_b[i])
+                list_tuple.append(resul)
+                aux += 1
+                break
+    new_tuple = tuple(list_tuple)
     return new_tuple
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     len_list = len(my_list)
-    max = 0
     if len_list == 0:
         return None
+    max = my_list[0]
     for x in my_list:
         if x > max:
             max = x
