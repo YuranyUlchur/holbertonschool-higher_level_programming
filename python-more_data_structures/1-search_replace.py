53947f5d519c16b161716d8ee3d5f4cfeb712e1a
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-02 17:36:22 -0500

    add changes

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@ def search_replace(my_list, search, replace):
     new_element = my_list[:]
     for x in range(len(my_list)):
         if search == new_element[x]:
-            new_element[x] == replace
+            new_element[x] = replace
     return new_element
