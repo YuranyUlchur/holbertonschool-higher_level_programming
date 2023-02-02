20840f6460d7abe6589e9ab49f995efe9e4bef96
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-02 17:30:13 -0500

    add changes the file one

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    len_list = len(my_list)
     new_element = my_list[:]
-    for x in range(len_list):
+    for x in range(len(my_list)):
         if search == new_element[x]:
             new_element[x] == replace
     return new_element
