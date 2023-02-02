8678e28eef0302e2c6be80273276ea6ff0f907f6
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-02 12:28:46 -0500

    add coditional the functions

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,19 +2,24 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list_tuple = []
     aux = 0
+    if len(tuple_a) == 0 and len(tuple_b) == 0:
+        return (0, 0)
     if len(tuple_a) == 0:
         return tuple_b
     if len(tuple_b) == 0:
         return tuple_a
-    if len(tuple_b) == 1:
+    if len(tuple_a) == 1:
+        list_tuple.append(tuple_b[0] + tuple_a[0])
+        if len(tuple_b) > 1:
+            list_tuple.append(tuple_b[1] + 0)
+        else:
+            list_tuple.append(0 + 0)
+    elif len(tuple_b) == 1:
         list_tuple.append(tuple_a[0] + tuple_b[0])
         list_tuple.append(tuple_a[1] + 0)
-    elif len(tuple_a) == 1:
-        list_tuple.append(tuple_b[0] + tuple_a[0])
-        list_tuple.append(tuple_b[1] + 0)
     else:
-        for x in range(0, len(tuple_a)):
-            for i in range(aux, len(tuple_b)):
+        for x in range(0, 2):
+            for i in range(aux, 2):
                 resul = (tuple_a[x] + tuple_b[i])
                 list_tuple.append(resul)
                 aux += 1
