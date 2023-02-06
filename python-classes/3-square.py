4846a32d169437b77f226e5907d434d2bf51a8a4
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-06 18:28:33 -0500

    class Square that defines a square tree

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square that defines a square"""
+
+
+class Square:
+    """Instantiation with size"""
+    def __init__(self, size=0):
+        self.__size = size
+        """size must be an integer"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    """Public instance method"""
+    def area(self):
+        return(self.__size * 5)
