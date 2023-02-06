9a479defc7d473fea183e3b894080086340c7efe
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-06 15:48:38 -0500

    add space

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """ an empty class Square that defines a square"""
+
+
 class Square:
     pass
+
+
 """ an empty class Square that defines a square"""
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Square that defines a square"""
+
+
 class Square:
     """Instantiation with size"""
     def __init__(self, size):
