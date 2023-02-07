7f1915791ab4c0260ff30c76337ecccd66aa6009
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-06 21:19:21 -0500

    class Square that defines a square by

---
+++ python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Square that defines a square"""
+
+
+class Square:
+    """Instantiation with size"""
+    def __init__(self, size=0):
+        self.size = size
+
+    """retrieve it"""
+    @property
+    def size(self):
+        return (self.__size)
+
+    """property setter to set it"""
+    @size.setter
+    def size(self, size):
+        self.__size = size
+        """size must be an integer"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        """Public instance method"""
+    def area(self):
+        return(self.__size ** 2)
