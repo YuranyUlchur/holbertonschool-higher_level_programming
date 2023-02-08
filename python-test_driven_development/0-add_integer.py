e03e57cd054c85eea4a9f8dc368abf5154e6cf92
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-08 18:19:54 -0500

    add comments

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    '''comparison of int and float'''
     if(type(a) != int and type(a) != float):
         raise TypeError("a must be an integer")
     if(type(b) != int and type(b) != float):
         raise TypeError("b must be an integer")
+    '''Returns an integer'''
     return(int(a + b))
