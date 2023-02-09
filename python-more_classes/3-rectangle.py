23fb5618ce398f123080d25629e6f142e60a8913
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-09 13:43:08 -0500

    the functions with rectangle with the character #

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -42,3 +42,13 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return(self.__perimeter)
+
+    ''' print the rectangle with the character #'''
+    def __str__(self):
+        aux = ""
+        for i in range(0, self.__height):
+            for x in range(0, self.__width):
+                aux += "#"
+            if i != self.__height - 1:
+                aux += "\n"
+        return aux
