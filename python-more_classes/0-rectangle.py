d12894dee8951261a06ac30ef1cac0dfca9dcacf
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-09 11:58:30 -0500

    class Rectangle that defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+''' that defines a rectangle'''
+
+
+class Rectangle:
+    '''initialization'''
+    pass
---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+'''class Rectangle that defines a rectangle'''
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+        """width must be an integer"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @width.setter
+    def height(self, value):
+        self.__height = value
+        """height must be an integer"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
