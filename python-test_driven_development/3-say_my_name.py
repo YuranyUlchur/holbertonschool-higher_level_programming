6cb91f6d8f40bf524872467176d62ff5f5fefc58
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-10 15:09:37 -0500

    add Doctest

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+'''function that prints My name is <first name> <last name>'''
+
+
 def say_my_name(first_name, last_name=""):
+    '''codition for chains'''
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
