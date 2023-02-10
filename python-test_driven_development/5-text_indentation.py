6ae7aaf458e4585801aeff9d35b0b1681cc6b10b
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-10 16:45:07 -0500

    add changes

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
+'''Text indentation functions'''
+
+
 def text_indentation(text):
-    
+    '''print the character'''
+
+    character = [".", ":", "?"]
+    aux = 0
+
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    for i in range(len(text)):
+        if i == len(text) - 1:
+            print(text[aux:i + 1], end="")
+        elif text[i] in character:
+            print(text[aux:i + 1], end="")
+            print('\n')
+            aux = i + 1
+            while text[aux] == " ":
+                aux += 1
