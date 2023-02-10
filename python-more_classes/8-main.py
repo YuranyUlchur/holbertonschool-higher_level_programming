346e34856717c50eeaf72a87ffc494b9796edf62
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-09 21:15:16 -0500

    changes the condition

--- python-more_classes/8-main.py
+++ python-more_classes/8-main.py
@@ -2,17 +2,9 @@
 Rectangle = __import__('8-rectangle').Rectangle
 
 my_rectangle_1 = Rectangle(8, 4)
-my_rectangle_2 = Rectangle(2, 3)
+my_rectangle_2 = Rectangle(1, 8)
 
-if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
-    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
-else:
-    print("my_rectangle_2 is bigger than my_rectangle_1")
-
-
-my_rectangle_2.width = 10
-my_rectangle_2.height = 5
-if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
-    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
-else:
-    print("my_rectangle_2 is bigger than my_rectangle_1")
+try:
+    print(my_rectangle_2 == Rectangle.bigger_or_equal(my_rectangle_1, "Rect"))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -71,8 +71,8 @@ class Rectangle:
 
     '''the biggest rectangle based on the area'''
     def bigger_or_equal(rect_1, rect_2):
-        if rect_1 == Rectangle:
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if rect_2 == Rectangle:
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         return rect_1
