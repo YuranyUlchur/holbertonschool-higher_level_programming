299c1936d5c9ccb97768aba3415441e8152ce9f0
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-10 15:51:28 -0500

    add changes the codition the size < 0

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -6,7 +6,7 @@ def print_square(size):
     '''codition the size'''
     if type(size) != int:
         raise TypeError("size must be an integer")
-    if size < 0:
+    elif size < 0:
         raise TypeError("size must be >= 0")
     for x in range(size):
         print("#" * size)
