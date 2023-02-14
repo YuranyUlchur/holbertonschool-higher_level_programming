3730fe7fc763f851994000a2b8d2a33f09cbd08b
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-14 18:04:14 -0500

    add space

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,6 +6,7 @@ class BaseGeometry:
     ''' validation'''
     def area(self):
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
