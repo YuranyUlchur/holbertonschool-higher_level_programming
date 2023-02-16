d8c80a1e14b50174d3c2c88a961503cede7de5e0
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-16 13:40:04 -0500

    add changes

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,15 +5,13 @@ import sys
 '''import functions'''
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_file = __import__('6-load_from_json_file.py').load_from_json_file
-''' script that adds all arguments '''
-
 
 args = sys.argv
 filename = "add_item.json"
+''' script that adds all arguments '''
 
 
 def main():
-
     '''add arguments to a list and save to filename'''
     try:
         my_list = load_file(filename)
