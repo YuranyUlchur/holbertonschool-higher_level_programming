b943139705267d98d8a16954f41bf3a0eec7f105
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-16 15:16:09 -0500

    the functions returns the dictionary

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''returns the dictionary description with simple data structure'''
+
+
+def class_to_json(obj):
+    '''returns the dictionary'''
+    dictionary = obj.__dict__
+    return dictionary
