37ad49a8ae7f77a78858a6962cc4e120af78d0d7
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-16 11:03:23 -0500

    'returns an object represented by a JSON string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''import json'''
+import json
+''' function JSON'''
+
+
+def from_json_string(my_str):
+    '''returns an object represented by a JSON string'''
+    return (json.dumps(my_str))
