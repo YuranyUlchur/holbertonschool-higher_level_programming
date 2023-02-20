0987d0dbb2132c1672129b41f834cc029039af4d
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-20 14:15:06 -0500

    add documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    '''Class constructor'''
     def __init__(self, width, height, x=0, y=0, id=None):
         ''' this super call with use the logic of the init of the Base class'''
         super().__init__(id)
