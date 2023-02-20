d0b7c5e2a366f63618e7b0729c87296f23fc152c
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-20 11:58:34 -0500

    add changes the functions

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,10 +5,11 @@
 class Base:
     '''private attribute'''
     __nb_objects = 0
+
     def __init__(self, id=None):
         '''validation'''
         if id is not None:
             self.id = id
         else:
-            Base.nb_objects += 1
-            self.id = Base.nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
