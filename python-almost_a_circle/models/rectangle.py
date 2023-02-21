6df5dbf36dac5337a71892e81f943219c85486de
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-21 13:03:13 -0500

    add validation the argument in the settter

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,10 +9,10 @@ class Rectangle(Base):
         ''' this super call with use the logic of the init of the Base class'''
         super().__init__(id)
         '''Assign each argument '''
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     ''' access the attribute value.'''
     @property
@@ -23,7 +23,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         self.__width = value
-        return self.__width
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
 
     ''' access the attribute value.'''
     @property
@@ -34,7 +37,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         self.__height = value
-        return self.__height
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
 
     ''' access the attribute value.'''
     @property
@@ -45,7 +51,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         self.__x = value
-        return self.__x
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
 
     ''' access the attribute value.'''
     @property
@@ -56,4 +65,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         self.__y = value
-        return self.__y
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
