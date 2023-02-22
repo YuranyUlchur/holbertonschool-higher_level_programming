25c7e4e9e4faf67c991ae45c95cf9895d8d031fe
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-22 16:10:33 -0500

    add documentation

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+'''import the base class from the module'''
 from models.rectangle import Rectangle
 
-class Square(Rectangle):
 
+class Square(Rectangle):
+    '''Class constructor'''
     def __init__(self, size, x=0, y=0, id=None):
+        ''' this super call with use the logic of the init of the Base class'''
         super().__init__(size, size, x, y, id)
-    
+
+    ''' access the attribute value.'''
     @property
     def size(self):
         return self.width
 
+    '''update the value of the attribute'''
     @size.setter
     def size(self, value):
         self.width = value
@@ -19,14 +24,15 @@ class Square(Rectangle):
         elif value <= 0:
             raise ValueError("width must be > 0")
 
-
+    '''represents an object'''
     def __str__(self):
         return ("[Square] ({}) {}/{} - {}"
                 .format(self.id, self.x, self.y,
                         self.width))
 
+    '''assigns an argument to each attribute'''
     def update(self, *args, **kwargs):
-
+        ''' assigns'''
         for i in range(len(args)):
             if i == 0:
                 self.id = args[i]
@@ -38,6 +44,7 @@ class Square(Rectangle):
                 self.y = args[i]
         if len(args) == 0:
             pass
+        '''assigns a key/value argument to attributes'''
         for key, value in kwargs.items():
             if key == "id":
                 self.id = value
