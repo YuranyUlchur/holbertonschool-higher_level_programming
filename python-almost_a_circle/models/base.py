53ec82b16bd54f508eb440497828039cb849083f
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-23 14:17:17 -0500

    delte space

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,7 +19,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         '''returns'''
-        len_dic = len(list_dictionaries)
-        if list_dictionaries is None or len_dic == 0:
-            return "[]"
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return []
         return json.dumps(list_dictionaries)
