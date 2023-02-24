fc85d1feb87c6cf7ec25c2c5aaa304f8a515d163
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-24 15:24:09 -0500

    new Module of unit tests for the Base class.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return"[]"
+            return []
         return json.dumps(list_dictionaries)
 
     '''writes the JSON string representation'''
