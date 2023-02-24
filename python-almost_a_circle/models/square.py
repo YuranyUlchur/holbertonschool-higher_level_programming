fd8a055a22e78381b3a84d867d739e297714ac8c
YuranyUlchur <ulchuryurani80@gmail.com>
2023-02-24 17:36:35 -0500

    add changes the class square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,64 +1,44 @@
 #!/usr/bin/python3
-'''import the base class from the module'''
+"""create class Rectangle"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    '''Class constructor'''
+    """the class Square that inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        ''' this super call with use the logic of the init of the Base class'''
+
         super().__init__(size, size, x, y, id)
 
-    ''' access the attribute value.'''
+    def __str__(self):
+        """String representation of a square."""
+        return ("[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width))
+
     @property
     def size(self):
+        """The size of the square."""
         return self.width
 
-    '''update the value of the attribute'''
     @size.setter
     def size(self, value):
         self.width = value
         self.height = value
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width must be > 0")
 
-    '''represents an object'''
-    def __str__(self):
-        return ("[Square] ({}) {}/{} - {}"
-                .format(self.id, self.x, self.y,
-                        self.width))
-
-    '''assigns an argument to each attribute'''
     def update(self, *args, **kwargs):
-        ''' assigns'''
-        for i in range(len(args)):
-            if i == 0:
-                self.id = args[i]
-            elif i == 1:
-                self.width = args[i]
-            elif i == 2:
-                self.x = args[i]
-            elif i == 3:
-                self.y = args[i]
-        if len(args) == 0:
-            pass
-        '''assigns a key/value argument to attributes'''
-        for key, value in kwargs.items():
-            if key == "id":
-                self.id = value
-            elif key == "size":
-                self.size = value
-            elif key == "x":
-                self.x = value
-            elif key == "y":
-                self.y = value
+        """that assigns an argument to each attribute"""
+        if args:
+            order = ["id", "size", "x", "y"]
+            for index, arg in enumerate(args):
+                setattr(self, order[index], arg)
 
-    '''returns the dictionary representation of a Square'''
+        elif kwargs:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+
     def to_dictionary(self):
-        '''return'''
-        return {"id": self.id,
-                "x": self.x,
-                "size": self.size,
-                "y": self.y}
+        """that returns the dictionary representation of a Square"""
+        return {'id': self.id,
+                'size': self.size,
+                'x': self.x,
+                'y': self.y}
