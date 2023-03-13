50b9e7cd5c35b29b1c1eac2172f12591568986a9
YuranyUlchur <ulchuryurani80@gmail.com>
2023-03-13 16:13:03 -0500

    add  First table

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
--- List tables
+-- List tablescat 3-list_tables.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 SHOW TABLES;
---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- First table
+CREATE TABLE first_table( id INT, name  VARCHAR(256))
