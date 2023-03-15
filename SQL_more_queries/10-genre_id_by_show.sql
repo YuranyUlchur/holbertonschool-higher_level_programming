b9d907cbe1c682b14a5f7822f64e50eec843f818
YuranyUlchur <ulchuryurani80@gmail.com>
2023-03-15 17:13:04 -0500

    add changes

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- Cities by States
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows 
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.id
+FROM tv_shows AS tv_shows
+JOIN tv_show_genres AS tv_show_genres 
+ON tv_shows.id = tv_show_genres.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
