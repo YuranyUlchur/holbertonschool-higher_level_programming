8be34673b0c8d038b41e45682042c6e5a14440da
YuranyUlchur <ulchuryurani80@gmail.com>
2023-03-15 11:46:15 -0500

    table creation with 2 columns id and name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- creates the table force_name on your MySQL server.
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT,
     name VARCHAR(256) NOT NULL
 );
