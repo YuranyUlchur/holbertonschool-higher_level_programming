6241af8ab35fbe39bdffa84d4627c39356ddad6c
YuranyUlchur <ulchuryurani80@gmail.com>
2023-03-15 18:28:41 -0500

    add el id de tv show y genre

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
 -- Cities by States
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows AS tv_shows
-JOIN tv_show_genres AS tv_show_genres 
-ON tv_shows.id = tv_show_genres.id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+use hbtn_0d_tvshows;
+SELECT sh.title, sg.genre_id
+FROM tv_shows AS sh 
+JOIN tv_show_genres AS sg
+ON sh.id = sg.show_id
+ORDER BY sh.title, sg.genre_id ASC;
