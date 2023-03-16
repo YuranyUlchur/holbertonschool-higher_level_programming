c978632d6b6db09a7bf35bfc932a59fc99ad2c6b
YuranyUlchur <ulchuryurani80@gmail.com>
2023-03-16 12:52:00 -0500

    lists all programs, and all genres linked to that program

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- List shows and genres
+SELECT st.title, g.name
+FROM tv_shows AS st
+LEFT JOIN tv_show_genres AS sh
+ON st.id = sh.show_id
+LEFT JOIN tv_genres AS g
+ON g.id = sh.genre_id
+ORDER BY st.title, g.name ASC;
