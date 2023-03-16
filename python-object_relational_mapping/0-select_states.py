c585fac3d2df43d47732b7004ee70d0ac0980996
YuranyUlchur <ulchuryurani80@gmail.com>
2023-03-16 17:20:03 -0500

    add comments

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""script that lists all states from the database"""
+"""script that lists all states from the database
+"""
+
 
 import MySQLdb
 import sys
-"""main performance condition"""
 
 if __name__ == "__main__":
-    """define attributes take 3 arguments"""
+
     username = sys.argv[1]
     passwd = sys.argv[2]
     database = sys.argv[3]
 
-    """script lists"""
-conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
