1fab9d1f9b445f5bf9b9eab1a98192ec3e750220
YuranyUlchur <ulchuryurani80@gmail.com>
2023-03-16 17:29:20 -0500

    add comments

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,11 +12,19 @@ if __name__ == "__main__":
     passwd = sys.argv[2]
     database = sys.argv[3]
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
+    conn = MySQLdb.connect(
+        host="localhost",
+        port = 3306,
+        user = username,
+        passwd = password,
+        db = database,
+        charset="utf8"
+    )
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+
+    for row in cur.execute:
         print(row)
+
     cur.close()
     conn.close()
