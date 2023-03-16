e88c5d45c376f619243813dc8a541bee7004e806
YuranyUlchur <ulchuryurani80@gmail.com>
2023-03-16 12:10:42 -0500

    add changes the file 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,10 @@
 -- My genres
-SELECT g.name
-FROM tv_genres AS g
-JOIN tv_show_genres AS sg
-ON g.id = sg.genre_id
-JOIN tv_show AS sh
-ON sh.id = sg.show_id
-WHERE sh.title = "Dexter"
-ORDER BY g.name;
+SELECT gen.name
+FROM tv_genres AS gen
+JOIN tv_show_genres AS sh
+ON gen.id = sh.genre_id
+JOIN tv_shows AS sh
+ON sh.id = rel.show_id
+WHERE sh.show_id = (SELECT id FROM tv_shows WHERE title = "Dexter")
+ORDER BY gen.name;
 
