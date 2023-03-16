f8c07baa7b2cafa514fb4c2c6a9ee685e8cdab3e
YuranyUlchur <ulchuryurani80@gmail.com>
2023-03-15 21:13:35 -0500

    Number of shows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,9 @@
 -- Number of shows by genre
 -- GROUP BY clause to group the results by genre
 -- HAVING clause is used to filter out genres
-SELECT tv_genre.name AS genre, COUNT(tv_genre.id) AS number_of_shows
-FROM tv_show_genres
-GROUP BY genre
-HAVING COUNT(*) > 0
+SELECT gen.name AS genre, COUNT(gen.id) AS number_of_shows
+FROM tv_genres AS gen
+JOIN tv_show_genres AS sh
+    ON gen.id = sh.genre_id
+GROUP BY gen.id
 ORDER BY number_of_shows DESC;
