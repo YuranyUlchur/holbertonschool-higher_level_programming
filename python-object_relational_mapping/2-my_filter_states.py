5ff06a4dd42ca6637b84e0d6e80483de2dbce80b
YuranyUlchur <ulchuryurani80@gmail.com>
2023-03-16 20:29:12 -0500

    add script that takes in an argument and displays all values in the states table

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values in the states table
+"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    matchname = sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{matchname}'")
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+    cursor.close()
+    conn.close()
