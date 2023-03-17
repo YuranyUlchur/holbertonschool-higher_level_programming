2fa88eab29b0242d783b2ede8af35c1fc6917d1f
YuranyUlchur <ulchuryurani80@gmail.com>
2023-03-16 21:54:10 -0500

    add BINARY  in code

--- python-object_relational_mapping/0-select_states.sql
+++ python-object_relational_mapping/0-select_states.sql
@@ -1,9 +1,10 @@
--- Create states table in hbtn_0e_0_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
-USE hbtn_0e_0_usa;
+-- Database + tables to test
+DROP DATABASE IF EXISTS test_2;
+CREATE DATABASE IF NOT EXISTS test_2;
+USE test_2;
 CREATE TABLE IF NOT EXISTS states ( 
     id INT NOT NULL AUTO_INCREMENT, 
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+INSERT INTO states (name) VALUES ("California"), ("nevada"), ("Nevada"), ("neVAda"), ("New York");
