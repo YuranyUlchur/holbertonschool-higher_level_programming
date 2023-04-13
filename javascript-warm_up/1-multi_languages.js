4b3e3ea956c7b7679eaf8d49ab8373f8a8c76516
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-12 20:12:18 -0500

    add files for print an string and wiht argument

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+//script that prints 3 lines
+const line1 = "C is fun";
+console.log(line1);
+
+const line2 = "Python is cool";
+console.log(line2);
+
+const line3 = "JavaScript is amazing";
+console.log(line3);
