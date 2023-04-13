b1a3c5af4a49c2e489aa2e33c748ac8a5429eb43
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-12 20:26:09 -0500

    ejecute  semistandard --fix

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // 'script that prints 3 lines'
 
-const line1 = "C is fun";
+const line1 = 'C is fun';
 console.log(line1);
 
-const line2 = "Python is cool";
+const line2 = 'Python is cool';
 console.log(line2);
 
-const line3 = "JavaScript is amazing";
+const line3 = 'JavaScript is amazing';
 console.log(line3);
--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,12 +3,12 @@
 // 'script en la línea de comandos'
 // 'length: número total de elementos'
 
-const numArgs = process.argv.length - 2; 
+const numArgs = process.argv.length - 2;
 
 if (numArgs === 0) {
-    console.log("No argument");
-  } else if (numArgs === 1) {
-    console.log("Argument found");
-  } else {
-    console.log("Arguments found");
-  }
+  console.log('No argument');
+} else if (numArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
