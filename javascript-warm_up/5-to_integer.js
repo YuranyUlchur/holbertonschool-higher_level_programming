25625e19634d882756cb43900292e3a52901c5d6
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-13 20:45:59 -0500

    add new files of script that prints My number and in bucle

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+//  An Integer
+const arg1 = process.argv[2];
+const arg = parseInt(arg1, 10);
+
+if (!isNaN(arg)) {
+  console.log('My number: ' + arg);
+} else {
+
+  console.log('Not a number');
+}
