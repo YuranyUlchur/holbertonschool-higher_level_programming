c824945a3fd517aaeb01905ea091d18231dfe3b4
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-13 21:18:38 -0500

    add new file what script that prints a square and script that prints x times “C is fun”

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// script that prints a square
+
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
