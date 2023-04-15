f6fa4df7b1c152bab99eaaeed5a69f58b9be1f8a
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-14 19:27:59 -0500

    add changes in fuctions the file

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 // script that computes and prints a factorial
 
-const factorial = (n) => {
+function computeFactorial (n) {
   if (isNaN(n)) {
     return 1;
-  } else if (n <= 1) {
+  } else if (n < 0) {
+    return NaN;
+  } else if (n === 0 || n === 1) {
     return 1;
   } else {
-    return n * factorial(n - 1);
+    return n * computeFactorial(n - 1);
   }
-};
+}
 
-const argument = parseInt(process.argv[2]);
-const result = factorial(argument);
+const n = parseInt(process.argv[2]);
 
-console.log(result);
+console.log(computeFactorial(n));
