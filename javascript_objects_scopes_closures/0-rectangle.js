8006982cac94f44b9d150e2e43dce657545b3cd6
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-17 10:58:59 -0500

    create file for add class rectangle and define

---
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Empty class Rectangle
+// exporting an anonymous class
+module.exports = class Rectangle {
+};
---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// defines a rectangle:
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
