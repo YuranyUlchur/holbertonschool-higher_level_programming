f4e084de6eb295857db0c55e2ef7d04bf37aa7f7
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-18 14:09:58 -0500

    add new file what script that reads and prints the content of a file.

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file
+const fs = require('fs');
+if (process.argv.length < 3) {
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
