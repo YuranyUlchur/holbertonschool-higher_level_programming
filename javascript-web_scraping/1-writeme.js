f1052d7e42b8579926a1a946980856113244f813
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-19 15:28:34 -0500

    add changes in files

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 // script that write and prints the content of a file
 const fs = require('fs');
-if (process.argv.length < 4) {
-  process.exit(1);
-}
-
 const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
 fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
   if (err) {
-    console.log('Python is cool');
-  } else {
     console.log(err);
   }
 });
