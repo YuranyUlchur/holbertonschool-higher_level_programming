c62508febabb23d3b785020842cbc694d6c487bd
YuranyUlchur <ulchuryurani80@gmail.com>
2023-04-20 18:44:51 -0500

    create new files donde add an class, Alternate classes and add an element to an list

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// updates the text color of the header
+$('#toggle_header').click(function () {
+  $('header').toggleClass('red green');
+});
---
+++ javascript-web_jquery/5-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// updates the text color of the header
+$('#add_item').click(function () {
+  $('.my_list').append('<li>Item</li>');
+});
